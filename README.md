b7cce2c0f2ca165652c19444df277536e510fb49
Vincent DELOFFRE <kratos_angel_59@hotmail.fr>
2023-04-12 20:27:09 +0200

    00_journalists v1

---
+++ README.md
@@ -0,0 +1,9 @@
+data_scientist_THP
+
+├── lib
+
+│   ├── 00_journalists.rb
+
+│   └── 01_cryptocurrencies.rb
+
+└── README.md
---
+++ lib/00_journalists.rb
@@ -0,0 +1,73 @@
+require_relative "./sources.rb"
+
+def put_rep(list)
+    i = 4
+
+    time = list.max_by(&:length).length
+    time.times do
+        if (list.any?{|word|word.length == i})
+            puts ">>>>>>> il y a #{list.select{|word|word.length == i}.count} mots de #{i} caractères de long."
+        end
+        i += 1
+    end
+end
+
+def is_length(list)
+    answer = ""
+
+    while answer != "oui" && answer != "non"
+        answer = gets.chomp
+    end
+    if answer == "oui"
+        list = list.sort_by(&:length)
+        answer = ""
+        puts ">>>>>>> Voulez-vous print la nouvelle liste des handle ?" 
+        while answer != "oui" && answer != "non"
+            answer = gets.chomp
+        end
+        if answer == "oui"
+            print "#{list}\n"
+        end
+    end
+    return list
+end
+
+def is_alpha(list)
+    answer = ""
+
+    while answer != "oui" && answer != "non"
+        answer = gets.chomp
+    end
+    if answer == "oui"
+        list = list.sort
+        answer = ""
+        puts ">>>>>>> Voulez-vous print la nouvelle liste des handle ?" 
+        while answer != "oui" && answer != "non"
+            answer = gets.chomp
+        end
+        if answer == "oui"
+            print "#{list}\n"
+        end
+    end
+    return list
+end
+
+def perform
+    handle = handle_array
+    save = handle
+    
+    puts ">> Il y a #{handle.count} handle dans cette array."
+    puts ">>> Le handle le plus court est : #{handle.min_by(&:length)}."
+    puts ">>>> Il y a #{handle.select{|word|word.length == 6 }.count} handle contenant 5 caractères."
+    puts ">>>>> Il y a #{handle.select{|word|word=~/^@[A-Z]/}.count} handle dont le nom commence par une majuscule."
+    puts ">>>>>> Voulez-vous trier les handle par ordre alphabétique ? oui/non"
+    handle = is_alpha(handle)
+    puts ">>>>>>> Les handle ont été trié par ordre alphabétique"
+    puts ">>>>>>>> Voulez-vous trier les handle par ordre croissant? oui/non"
+    handle = is_length(handle)
+    puts ">>>>>>>>> Les handle ont été trié par ordre croissant"
+    puts ">>>>>>>>>> La position de @epenser est la #{handle.index('@epenser')}"
+    put_rep(handle)
+end
+
+perform
