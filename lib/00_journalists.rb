fe24ecd0fcb7234868bf7310b613ceaa0a1f956b
Vincent DELOFFRE <kratos_angel_59@hotmail.fr>
2023-04-13 09:30:18 +0200

    mise en forme de 00_journalists.rb

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -6,7 +6,7 @@ def put_rep(list)
     time = list.max_by(&:length).length
     time.times do
         if (list.any?{|word|word.length == i})
-            puts ">>>>>>> il y a #{list.select{|word|word.length == i}.count} mots de #{i} caractères de long."
+            puts ">>>>>>>>> il y a #{list.select{|word|word.length == i}.count} handle(s) de #{i} caractères de long."
         end
         i += 1
     end
@@ -16,6 +16,7 @@ def is_length(list)
     answer = ""
 
     while answer != "oui" && answer != "non"
+        print ">>>>>>> "
         answer = gets.chomp
     end
     if answer == "oui"
@@ -23,10 +24,11 @@ def is_length(list)
         answer = ""
         puts ">>>>>>> Voulez-vous print la nouvelle liste des handle ?" 
         while answer != "oui" && answer != "non"
+            print ">>>>>>> "
             answer = gets.chomp
         end
         if answer == "oui"
-            print "#{list}\n"
+            print "\n#{list}\n"
         end
     end
     return list
@@ -36,17 +38,19 @@ def is_alpha(list)
     answer = ""
 
     while answer != "oui" && answer != "non"
+        print ">>>>>> "
         answer = gets.chomp
     end
     if answer == "oui"
         list = list.sort
         answer = ""
-        puts ">>>>>>> Voulez-vous print la nouvelle liste des handle ?" 
+        puts ">>>>>> Voulez-vous print la nouvelle liste des handle ?" 
         while answer != "oui" && answer != "non"
+            print ">>>>>> "
             answer = gets.chomp
         end
         if answer == "oui"
-            print "#{list}\n"
+            print "\n#{list}\n"
         end
     end
     return list
@@ -56,17 +60,17 @@ def perform
     handle = handle_array
     save = handle
     
-    puts ">> Il y a #{handle.count} handle dans cette array."
+    puts ">> Il y a #{handle.count} handle(s) dans cette array."
     puts ">>> Le handle le plus court est : #{handle.min_by(&:length)}."
-    puts ">>>> Il y a #{handle.select{|word|word.length == 6 }.count} handle contenant 5 caractères."
-    puts ">>>>> Il y a #{handle.select{|word|word=~/^@[A-Z]/}.count} handle dont le nom commence par une majuscule."
-    puts ">>>>>> Voulez-vous trier les handle par ordre alphabétique ? oui/non"
+    puts ">>>> Il y a #{handle.select{|word|word.length == 6 }.count} handle(s) contenant 5 caractères."
+    puts ">>>>> Il y a #{handle.select{|word|word=~/^@[A-Z]/}.count} handle(s) dont le nom commence par une majuscule."
+    puts ">>>>>> Voulez-vous trier les handles par ordre alphabétique ? oui/non"
     handle = is_alpha(handle)
-    puts ">>>>>>> Les handle ont été trié par ordre alphabétique"
-    puts ">>>>>>>> Voulez-vous trier les handle par ordre croissant? oui/non"
+    puts ">>>>>> Les handles ont été trié par ordre alphabétique"
+    puts ">>>>>>> Voulez-vous trier les handles par ordre croissant? oui/non"
     handle = is_length(handle)
-    puts ">>>>>>>>> Les handle ont été trié par ordre croissant"
-    puts ">>>>>>>>>> La position de @epenser est la #{handle.index('@epenser')}"
+    puts ">>>>>>> Les handles ont été trié par ordre croissant"
+    puts ">>>>>>>> La position de @epenser est la #{handle.index('@epenser')}"
     put_rep(handle)
 end
 
